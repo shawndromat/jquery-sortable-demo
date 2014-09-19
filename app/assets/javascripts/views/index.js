fdf8c361a86a726bf95a31137da0f634de970bfe
Shawna <shawna@appacademy.io>
2014-09-19 08:19:27 -0700

    skeleton

--- app/assets/javascripts/views/index.js
+++ app/assets/javascripts/views/index.js
@@ -3,6 +3,9 @@ JqueryUi.Views.Index = Backbone.CompositeView.extend({
     this.listenTo(this.collection, "sync", this.render);
     this.listenTo(this.collection, "add", this.addList)
   },
+  events: {
+    "sortstart #lists-wrapper": "printSubviews"
+  },
   template: JST["index"],
   render: function () {
     var content = this.template({lists: this.collection});
@@ -13,5 +16,8 @@ JqueryUi.Views.Index = Backbone.CompositeView.extend({
   addList: function (list) {
     var listShow = new JqueryUi.Views.ListShow({model: list});
     this.addSubview("#lists-wrapper", listShow)
+  },
+  printSubviews: function () {
+    console.log(this.subviews());
   }
 })
